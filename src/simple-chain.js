92995d184988dc629d0f1fccbc2f3219d6258069
Aksultan Aitkozhin <52781023+AksultanAitkozha@users.noreply.github.com>
2020-11-02 00:23:56 +0600

    docs: update chain-maker.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,32 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  array : [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(value !== undefined) this.array.push(value)
+    else this.array.push('');
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof(position) != "number" || position - 1 < 0 || position > this.array.length){
+       this.array = [];
+       throw Error();
+    }
+    this.array.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let str = "";
+    for(let i=0;i < this.array.length;i++) {
+      str = str+'( '+this.array[i]+' )~~';
+    }
+    this.array = [];
+    return str.slice(0, -2);
   }
 };
 
